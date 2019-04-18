db41e6cc25db3c3d6c0497626b360eb6620dd871
DarylB <dbotengan01@yahoo.com>
2019-04-18 15:12:56 -0600

    added background and design

--- script.js
+++ script.js
@@ -4,45 +4,62 @@ var grid = document.createElement("div");
 grid.className = "grid";
 body.appendChild(grid);
 
-var colors = ['#ff0000', '#00ff00', '#0000ff'];
+var colors = ["red box","green box", "blue box", "yellow box"];
 var anycolor = colors[Math.floor(Math.random() * colors.length)];
 
 var rowscols;
 
 var draw = document.querySelector("#draw")
-
-var erase = document.querySelector("#erase");
-    
-var clear = document.getElementsByClassName("blackbox");
-
+var erase = document.querySelector("#erase");  
+var clear = document.getElementsByClassName("box")
+var color = document.querySelector("#color");
+allbuttons = document.getElementsByClassName("button");
 var reset = document.querySelector("#reset");
     reset.addEventListener("click", () => {resetbutton()});
         function resetbutton() {
             for (i = 0; i < clear.length ; i++ ) {
-                clear[i].classList.add("gridbox");
+                clear[i].classList.remove("red");
+                clear[i].classList.remove("black");
+                clear[i].classList.remove("blue");
+                clear[i].classList.remove("yellow");
+                clear[i].classList.remove("green");
             }
 }
-function randomcolor() {
-    e.target.style.color = anycolor;
+function randomcolor(e) {
+    var anycolor = colors[Math.floor(Math.random() * colors.length)];
+    e.target.className = anycolor;
 }
-function changecolor(e) {
-    e.target.className = "blackbox";
+function drawblack(e) {
+    e.target.className = "black box";
 }
 function erasecolor(e) {
-    e.target.className = "gridbox";
+    e.target.className = "box";
 }
 
-var setbox = document.querySelector("#setbox");
-    setbox.addEventListener('click', () => {creategrid()});
+var setboxes = document.querySelector("#setbox");
+    setboxes.addEventListener('click', () => {creategrid()});
+    setboxes.addEventListener('click', () => {changepointer()})
+        function changepointer () {
+            body.style.cursor = "auto";
+            draw.style.cursor = "auto";
+            erase.style.cursor = "auto";
+            reset.style.cursor = "auto";
+            setboxes.style.cursor = "auto";
+            color.style.cursor = "auto";
+        }
 
 function creategrid () {
-    rowscols = prompt("How many boxes per side?");
-    if (rowscols > 50) {
-        alert("Please input a number less than 50");}
-    else if (rowscols < 1) {
-        alert("Please input a number more than 0");
+    rowscols = prompt("How many boxes per side? (Max : 20)");
+    if (rowscols === null) {
+        return;
     }
-    if (rowscols < 51 && rowscols > 0) {
+    if (rowscols > 21) {
+        alert("Please input a number less than 21");
+    }
+    else if (rowscols < 1) {
+        alert("Please input a number more than 0 (Max : 20)");
+    }
+    if (rowscols < 21 && rowscols > 0) {
         while (grid.hasChildNodes()){
             grid.removeChild(grid.lastChild);
             }   
@@ -51,18 +68,44 @@ function creategrid () {
             var gridnumber = rowscols * rowscols;
     for (i = 0; i < gridnumber; i ++) {
             let gridcol = document.createElement("div");
-            gridcol.className = "gridbox";
+            gridcol.className = "box";
             grid.appendChild(gridcol);        
-                draw.addEventListener('click', () => {startdrawing()});
-                function startdrawing() {
-                    gridcol.removeEventListener('mouseover', erasecolor)
-                    gridcol.addEventListener('mouseover', changecolor);
+                draw.addEventListener('click', () => {startDrawing()});
+                function startDrawing() {
+                    draw.style.cursor = "url('icons/pencil.png') 2 28, default";
+                    erase.style.cursor = "url('icons/pencil.png') 2 28, default";
+                    reset.style.cursor = "url('icons/pencil.png') 2 28, default";
+                    setboxes.style.cursor = "url('icons/pencil.png') 2 28, default";
+                    color.style.cursor = "url('icons/pencil.png') 2 28, default";
+                    body.style.cursor = "url('icons/pencil.png') 2 28, default";
+                    gridcol.removeEventListener('mouseover', randomcolor);
+                    gridcol.removeEventListener('mouseover', erasecolor);
+                    gridcol.addEventListener('mouseover', drawblack);
                 }
                 erase.addEventListener('click', () => {startErasing()});
                 function startErasing() {
-                    gridcol.removeEventListener('mouseover', changecolor);
+                    draw.style.cursor = "url('icons/eraser.png') 2 28, default";
+                    erase.style.cursor = "url('icons/eraser.png') 2 28, default";
+                    reset.style.cursor = "url('icons/eraser.png') 2 28, default";
+                    setboxes.style.cursor = "url('icons/eraser.png') 2 28, default";
+                    color.style.cursor = "url('icons/eraser.png') 2 28, default";
+                    body.style.cursor = "url('icons/eraser.png') 2 28, default";
+                    gridcol.removeEventListener('mouseover', randomcolor);
+                    gridcol.removeEventListener('mouseover', drawblack);
                     gridcol.addEventListener('mouseover', erasecolor);
-                }             
+                }
+                color.addEventListener('click', () => {startColoring()});
+                function startColoring() {
+                    draw.style.cursor = "url('icons/brush.png') 2 28, default";
+                    erase.style.cursor = "url('icons/brush.png') 2 28, default";
+                    reset.style.cursor = "url('icons/brush.png') 2 28, default";
+                    setboxes.style.cursor = "url('icons/brush.png') 2 28, default";
+                    color.style.cursor = "url('icons/brush.png') 2 28, default";
+                    body.style.cursor = "url('icons/brush.png') 2 28, default";
+                    gridcol.removeEventListener('mouseover', erasecolor);
+                    gridcol.removeEventListener('mouseover', drawblack);
+                    gridcol.addEventListener('mouseover', randomcolor);
+                }            
         }
     } 
 }
